68a53b24793a1983aa3395e772756d20334bc0f9
Vignesh Sarma K <vignesh.sarma@gmail.com>
2015-12-07 23:00:21 +0530

    my updates

--- personal/init.el
+++ personal/init.el
@@ -8,7 +8,7 @@
                sphinx-doc ag rvm ido-vertical-mode tern tern-auto-complete
                yaml-mode anaconda-mode helm helm-projectile helm-ag
                hl-sexp tuareg virtualenvwrapper smart-mode-line rich-minority
-               restclient yasnippet paradox
+               restclient yasnippet paradox beacon
 
                ;; themes
                noctilux-theme color-theme-sanityinc-tomorrow
@@ -41,9 +41,16 @@
 (require 'stylus-mode)
 (add-to-list 'auto-mode-alist '("\\.jade$" . stylus-mode))
 
-(set-frame-font "Monaco-12")
+;; (set-face-attribute 'default nil :height 125 :family "Source Code Pro")
+;; (set-frame-font "Fira-mono-12")
+;; (set-frame-font "Hack-12")
+(set-frame-font "Menlo-12")
+;; (set-face-attribute 'default nil :height 135 :family "Anonymous Pro")
+;; (set-frame-font "Consolas-13")
+;; (set-frame-font "Monaco-12")
+;; (set-frame-font "Inconsolata-14.5")
 (disable-theme 'zenburn)
-(load-theme 'subatomic)
+(load-theme 'arjen-grey)
 ;; (load-theme 'sanityinc-tomorrow-eighties)
 ;; (display-time)
 
@@ -61,7 +68,7 @@
 (setq ns-pop-up-frames nil)
 
 (global-set-key (kbd "s-A") 'ag-project-files)
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+(global-set-key (kbd "C-c SPC") 'avy-goto-char)
 (setq ag-highlight-search t)
 
 (setq flx-ido-threshold 1000)
@@ -82,7 +89,7 @@
 (require 'virtualenvwrapper)
 (venv-initialize-interactive-shells) ;; if you want interactive shell support
 (venv-initialize-eshell) ;; if you want eshell support
-(setq venv-location "/Users/vignesh/.venvs")
+(setq venv-location "/Users/vigneshS/.venvs")
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward uniquify-separator ":")
@@ -102,9 +109,6 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.sls$" . yaml-mode))
 
-;; emacs from comandline
-(x-focus-frame nil)
-
 ;; lisp
 (setq slime-default-lisp 'sbcl)
 (setq slime-contribs '(slime-fancy))
@@ -114,17 +118,34 @@
 (require 'color)
 (custom-set-faces
  `(hl-sexp-face ((t (:background ,(-> (face-attribute 'highlight :background)
-                                      (color-lighten-name 4)))))))
+                                      (color-darken-name 4)))))))
 (add-hook 'lisp-mode-hook 'hl-sexp-mode)
 (add-hook 'emacs-lisp-mode-hook 'hl-sexp-mode)
 (add-hook 'clojure-mode-hook 'hl-sexp-mode)
 
+(setq cider-test-infer-test-ns (lambda (ns)
+                                 (let ((test-ns (format "%s-tests" ns)))
+                                  (message test-ns)
+                                  test-ns)))
+(setq cider-test-show-report-on-success t)
+
 (require 'rich-minority)
 (rich-minority-mode 1)
 (setq rm-blacklist (mapconcat 'identity (list "ws" "guru" "company" "Pre") "\\|"))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.asp$" . web-mode))
+(setq-default c-basic-offset 2
+              tab-width 2
+              indent-tabs-mode 'f)
 ;; Org Mode
 
 (add-hook 'org-mode-hook
           (lambda ()
             (visual-line-mode)
             (whitespace-mode -1)))
+
+(setq-default indent-tabs-mode nil)
+
+;; svn find file projectile overide.
+(custom-set-variables '(projectile-svn-command "find . -type f -print0"))
